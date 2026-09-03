8d8dc20a94dfc50bf674fa1ecc72b10eeef2b9d6
shreyansh <shreyanshthakur2007@gmail.com>
2026-09-03 21:16:48 +0530

    project initialization

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+app.get('/',function(req,res){
+    res.send({
+        message:"app working"
+    })
+})
+
+
+app.listen(3000,function(){
+    console.log("server is running")
+})
